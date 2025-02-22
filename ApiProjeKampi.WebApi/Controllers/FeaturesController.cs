4ac1c1d8a6475063889449053442988a0b5cf71e
Fatih İlhan <fatilhan1507@gmail.com>
2025-02-22 15:36:01 +0300

    13. ders bitirildi.

---
+++ ApiProjeKampi.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,62 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Dtos.FeatureDtos;
+using ApiProjeKampi.WebApi.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        private readonly IMapper _mapper;
+        public FeaturesController(ApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _context.Features.ToList();
+            return Ok(_mapper.Map<List<ResultFeatureDto>>(values));
+        }
+
+        [HttpPost]
+        public IActionResult CreateFetaure(CreateFeatureDto createFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDto);
+            _context.Features.Add(value);
+            _context.SaveChanges();
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            _context.Features.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpGet("GetFeature")]
+        public IActionResult GetFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            return Ok(_mapper.Map<GetByIdFeatureDto>(value));
+        }
+
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(updateFeatureDto);
+            _context.Features.Update(value);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı");
+        }
+    }
+}
---
+++ ApiProjeKampi.WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,62 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Dtos.MessageDtos;
+using ApiProjeKampi.WebApi.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        private readonly IMapper _mapper;
+        public MessagesController(ApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var values = _context.Messages.ToList();
+            return Ok(_mapper.Map<List<ResultMessageDto>>(values));
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var value = _mapper.Map<Message>(createMessageDto);
+            _context.Messages.Add(value);
+            _context.SaveChanges();
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _context.Messages.Find(id);
+            _context.Messages.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpGet("GetMessage")]
+        public IActionResult GetMessage(int id)
+        {
+            var value = _context.Messages.Find(id);
+            return Ok(_mapper.Map<GetByIdMessageDto>(value));
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            var value = _mapper.Map<Message>(updateMessageDto);
+            _context.Messages.Update(value);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı");
+        }
+    }
+}
