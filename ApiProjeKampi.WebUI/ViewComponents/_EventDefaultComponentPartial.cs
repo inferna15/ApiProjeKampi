08bd0d2f0fb65d474496791feee1edb82b0c408f
inferna15 <fatilhan1507@gmail.com>
2025-05-14 19:18:45 +0300

    28. ders bitirildi.

---
+++ ApiProjeKampi.WebUI/ViewComponents/_EventDefaultComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using ApiProjeKampi.WebUI.Dtos.YummyEventDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _EventDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _EventDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7028/api/YummyEvents");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultYummyEventDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
